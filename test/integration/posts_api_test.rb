68bd3c3fef0f3235fac4abd368672e765de09465
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-06-01 16:58:58 -0400

    set up a new integration test for the post index route when there are actual posts in the database. test red because no posts set up yet

--- test/integration/posts_api_test.rb
+++ test/integration/posts_api_test.rb
@@ -12,7 +12,20 @@ class PostsApiTest < ActionDispatch::IntegrationTest
 			assert_equal json["posts"].length, 0
 		end
 	end
-  # test "the truth" do
-  #   assert true
-  # end
+
+	context "with some posts" do
+		setup do
+			5.times { FactoryGirl.create(:post) }
+		end
+
+		should "display all posts" do
+			get "/v1/posts"
+
+			assert_response :success
+
+			json = JSON.parse(response.body)
+
+			assert_equal json["posts"].length, 5
+		end
+	end
 end
