bf2025ac4f4889b29ba45ad1f618ef25cfc34c60
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-06-01 17:23:33 -0400

    write an integration test for creating new posts and seeing the results reflected in the api

--- test/integration/posts_api_test.rb
+++ test/integration/posts_api_test.rb
@@ -27,5 +27,25 @@ class PostsApiTest < ActionDispatch::IntegrationTest
 
 			assert_equal json["posts"].length, 5
 		end
+
+		should "add a new post and display it at the end of the index route" do
+			post "/v1/posts", params: { post: {
+				title: Faker::Lorem.sentence,
+				categories: Faker::Lorem.words.join(' '),
+				content: Faker::Lorem.paragraph	
+			}}
+
+			assert_response :success
+
+			json = JSON.parse(response.body)
+
+			assert_equal json["post"]["title"], Post.last.title
+
+			get "/v1/posts"
+
+			assert_equal json["posts"].length, 6
+
+			assert_equal json["posts"][5]["content"], Post.last.content
+		end
 	end
 end
